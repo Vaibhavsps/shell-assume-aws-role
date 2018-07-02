6c7a77b60388f0f22a2f20fc64678a1d0ea4c214
Eugene Starchenko <17835122+eugenestarchenko@users.noreply.github.com>
2018-07-02 12:09:36 +0300

    Update assume_role.sh
    update

--- assume_role.sh
+++ assume_role.sh
@@ -4,16 +4,16 @@ function assume {
   profile=$1
   role=$2
   token=$3
-  duration=900
+  duration=3600
   if echo "$4" | grep "^[0-9]\{3,4\}$" > /dev/null; then duration=$4; fi
-  mfa_device="$(aws --profile $profile --region $region iam list-mfa-devices --query MFADevices[0].SerialNumber --output text  )"
+  mfa_device="$(aws --profile $profile --region $region iam list-mfa-devices --query "MFADevices[0].SerialNumber" --output text)"
   result="$(aws --profile $profile --region $region \
     sts assume-role --role-arn "$role" \
     --serial-number "$mfa_device" --role-session-name "`whoami`" --duration-seconds "$duration" \
     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' --output text --token-code "$token" \
   )"
-  if [ $? -ne 0 ]; then 
-      echo "Failed to assume role" 1>&2; return 1; 
+  if [ $? -ne 0 ]; then
+      echo "Failed to assume role" 1>&2; return 1;
   else
     export AWS_ACCESS_KEY_ID="`echo $result | awk '{ print $1 }'`"
     export AWS_SECRET_ACCESS_KEY="`echo $result | awk '{ print $2 }'`"
